bc127c90743295c15b621227c623970fc31f032d
madeha <madehayoussof96@gmail.com>
2025-09-27 15:15:32 +0300

    chore: init nextjs 15 portfolio

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      colors: {
+        brand: "#1DA1F2",
+        black: {
+          DEFAULT: "#000",
+          100: "َ#000333",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
